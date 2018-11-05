8b4ec201a5d1a811841c973a06fa7028084f1eec
Incognito <jrh.JackSparrow042@yahoo.com>
2018-11-05 08:35:41 -0500

    Removed export control

--- src/main/java/com/incognito/tools/sorts/ui/MainWindow.java
+++ src/main/java/com/incognito/tools/sorts/ui/MainWindow.java
@@ -1,14 +1,3 @@
-/**
- * WARNING - This document contains technical data whose export is restricted
- * by the Arms Export Control Act (Title 22, U.S.C., Sec 2751, et seq.) or the
- * Export Administration Act of 1979 (Title 50, U.S.C., App. 2401 et seq), as
- * amended.  Violations of these export laws are subject to severe criminal
- * penalties.  Disseminate in accordance with provisions of DoD Directive
- * 5230.25.
- * <p>
- * DESTRUCTION NOTICE: Destroy by any method that will prevent disclosure of
- * contents or reconstruction of the document.
- */
 package com.incognito.tools.sorts.ui;
 
 import com.incognito.tools.sorts.algorithms.BubbleSort;
@@ -41,9 +30,6 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
-/**
- * Created by jahorton on 10/30/2018
- */
 public class MainWindow {
     private JPanel root;
     private JComboBox<Sort> cmbAlgorithm;
--- src/main/java/com/incognito/tools/sorts/ui/components/GraphicsPanel.java
+++ src/main/java/com/incognito/tools/sorts/ui/components/GraphicsPanel.java
@@ -1,14 +1,3 @@
-/**
- * WARNING - This document contains technical data whose export is restricted
- * by the Arms Export Control Act (Title 22, U.S.C., Sec 2751, et seq.) or the
- * Export Administration Act of 1979 (Title 50, U.S.C., App. 2401 et seq), as
- * amended.  Violations of these export laws are subject to severe criminal
- * penalties.  Disseminate in accordance with provisions of DoD Directive
- * 5230.25.
- * <p>
- * DESTRUCTION NOTICE: Destroy by any method that will prevent disclosure of
- * contents or reconstruction of the document.
- */
 package com.incognito.tools.sorts.ui.components;
 
 import com.incognito.tools.sorts.algorithms.Sort;
@@ -16,9 +5,6 @@ import com.incognito.tools.sorts.algorithms.Sort;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 
-/**
- * Created by jahorton on 10/30/2018
- */
 public class GraphicsPanel extends JPanel {
     private Sort sort = null;
     private boolean isPlaying = false;
